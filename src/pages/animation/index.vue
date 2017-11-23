b0a3ef7ae33e085bb44e8579e9e4945e679571d9
calamus0427 <1211994507@qq.com>
2017-11-23 10:41:26 +0800

    vue动画

--- src/pages/animation/index.vue
+++ src/pages/animation/index.vue
@@ -1,5 +1,39 @@
 <template>
-  <div>
-    animation working!
+  <div id="example-1">
+    <el-radio v-model="view" label="v-a">A</el-radio>
+    <el-radio v-model="view" label="v-b">B</el-radio>
+    <transition name="component-fade" mode="out-in">
+      <component v-bind:is="view"></component>
+    </transition>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      show:true,
+      view: 'v-a'
+    }
+  },
+  components: {
+    'v-a': {
+      template: '<div>Component A</div>'
+    },
+    'v-b': {
+      template: '<div>Component B</div>'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.component-fade-enter-active, .component-fade-leave-active {
+  transition: opacity .3s ease;
+}
+.component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active for below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
+
+
